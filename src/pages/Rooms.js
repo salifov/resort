b8ca5fb6091c4affe5c630e6f7325741e3249592
unknown <rmzslfv1@abv.bg>
2019-12-14 13:20:48 +0200

    Hero added

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
 
-export default function Rooms() {
-    return (
-        <div>
-            Hello from rooms
-        </div>
-    );
-}
+const Rooms = () => {
+    return <Hero hero="roomsHero"></Hero>;
+};
+
+export default Rooms;
