9e7cb7492d037b997856b3ae203ae06356726671
unknown <rmzslfv1@abv.bg>
2019-12-14 14:25:49 +0200

    Services added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
+import Services from '../components/Services';
 
 export default function Home() {
     return (
-    <Hero hero="defaultHero">
-        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-            <Link to="/rooms" className="btn-primary">our rooms</Link>
-        </Banner>
-    </Hero>
+    <React.Fragment>
+        <Hero hero="defaultHero">
+            <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn-primary">our rooms</Link>
+            </Banner>
+        </Hero>
+        <Services></Services>
+    </React.Fragment>
     );
 }
